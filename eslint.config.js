15399a75dfa53a81c18cf1912a8312fa90cee635
LunaMooncraft <nidawi.nootau@gmail.com>
2025-06-18 14:13:59 +0200

    ➕ add eslint (tslint) & 🔧 create eslint (flat) config & init linting

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import eslintPluginVue from 'eslint-plugin-vue';
+import ts from 'typescript-eslint';
+import vueParser from 'vue-eslint-parser';
+import tsParser from '@typescript-eslint/parser';
+
+const customRules = {
+  '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+  'vue/attribute-hyphenation': 'off',
+  'vue/multi-word-component-names': 'off',
+  'no-console': ['warn', { allow: ['info', 'warn', 'error'] }]
+};
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...eslintPluginVue.configs['flat/recommended'],
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: customRules,
+  }
+);
